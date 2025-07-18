7bf00abbdd6cc2e9871e072548e9ff497edb0d72
Harsh Dabas <iamharshdabas@gmail.com>
2025-07-18 16:05:37 +0530

    feat: add multiline typing support

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env bun
 
-import chalk from "chalk"
 import { Command } from "commander"
-import rl from "./utils/readline"
+import { cleanupAndExit, rlClearScreenDown, rlCursorTo, rlOnKeypress } from "./utils/readline"
+import { getHintedText } from "./utils/text"
 
 const program = new Command()
 
@@ -12,9 +12,43 @@ program
   .command("start")
   .description("Starts the typing test.")
   .action(() => {
-    rl.on("line", (line) => {
-      console.log(chalk.white(line))
+    const hintText = "readline.emitKeypressEvents(process.stdin)"
+    let userText = ""
+
+    const render = () => {
+      const output = getHintedText(hintText, userText)
+      rlCursorTo(0, 0)
+      rlClearScreenDown()
+      process.stdout.write(output)
+      const userLines = userText.split("\n")
+      const currentLine = userLines.length - 1
+
+      rlCursorTo(userLines[currentLine]?.length, currentLine)
+    }
+
+    rlOnKeypress((str, key) => {
+      if (key.ctrl && key.name === "c") {
+        cleanupAndExit()
+      }
+
+      switch (key.name) {
+        case "backspace":
+          userText = userText.slice(0, -1)
+          break
+        case "tab":
+          userText += "  "
+          break
+        case "return":
+          userText += "\n"
+          break
+        default:
+          userText += str
+      }
+
+      render()
     })
+
+    render()
   })
 
 program.parse()
--- src/utils/readline.ts
+++ src/utils/readline.ts
@@ -3,9 +3,33 @@ import readline from "node:readline"
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  terminal: false,
 })
-readline.cursorTo(process.stdout, 0, 0)
-readline.clearScreenDown(process.stdout)
+
+export const rlClearScreenDown = () => readline.clearScreenDown(process.stdout)
+export const rlCursorTo = (x = 0, y = 0) => readline.cursorTo(process.stdout, x, y)
+
+export const rlOnKeypress = (
+  callback: (
+    str: string | undefined,
+    key: {
+      name: string
+      ctrl: boolean
+    },
+  ) => void,
+) => {
+  if (process.stdin.isTTY) {
+    process.stdin.setRawMode(true)
+  }
+  readline.emitKeypressEvents(process.stdin)
+  process.stdin.on("keypress", callback)
+}
+
+export const cleanupAndExit = () => {
+  if (process.stdin.isTTY) {
+    process.stdin.setRawMode(false)
+  }
+  process.stdout.write("\nExiting...\n")
+  process.exit(0)
+}
 
 export default rl
